2e17b24f95765aecce811362323e60f562f1b86b
Чухутин Игорь <chukhutinigor@yandex.ru>
2026-02-20 05:36:49 +0300

    gg

---
+++ users/views.py
@@ -0,0 +1,13 @@
+from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter
+from .models import Payment
+from .serializers import PaymentSerializer
+
+class PaymentViewSet(viewsets.ModelViewSet):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_fields = ['paid_course', 'paid_lesson', 'payment_method']
+    ordering_fields = ['payment_date']
+    ordering = ['-payment_date']
